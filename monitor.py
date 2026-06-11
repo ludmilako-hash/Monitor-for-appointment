7bc306722a64486748a31b744993d13c4487ec33
ludmilako-hash <ludmilakorniiets@gmail.com>
2026-06-11 15:58:57 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,47 +1,23 @@
 import os
+import requests
+from checker import check_slots
 
-print("STARTING BOT")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-try:
-    import requests
-    print("REQUESTS OK")
-except Exception as e:
-    print("IMPORT ERROR:", e)
-    raise
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
+def send(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
 
-print("TOKEN:", "OK" if BOT_TOKEN else "MISSING")
-print("CHAT:", CHAT_ID)
-
-URL = "https://wizyty.uml.lodz.pl"
-
-def check():
-    r = requests.get(URL, timeout=30)
-    return r.text.lower()
-
-def send(text):
-    if not BOT_TOKEN or not CHAT_ID:
-        print("Missing credentials")
-        return
-
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={"chat_id": CHAT_ID, "text": text}
-    )
 
 if __name__ == "__main__":
-    try:
-        html = check()
-        print("SITE LOADED")
+    result = check_slots()
 
-        if "brak wolnych termin" not in html:
-            print("SLOTS POSSIBLE")
-            send("🚨 Возможны слоты в Łódź!")
-        else:
-            print("NO SLOTS")
-
-    except Exception as e:
-        print("ERROR:", e)
-        raise
+    if result:
+        send("📅 Найден возможный свободный термин в UM Łódź!")
+    else:
+        print("No slots")
